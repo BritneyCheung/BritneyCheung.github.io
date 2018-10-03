97b9402b0709e470a9d8eba56c59b94acbe9aeda
Britney <cbrittles@yahoo.com>
2018-10-03 16:09:14 +0000

    thunkable commit

--- java.js
+++ java.js
@@ -14,7 +14,7 @@ function myFunction() {
   
 //Timer
 // Set the date we're counting down to
-var countDownDate = new Date("June 5, 2019 12:0:0").getTime();
+var countDownDate = new Date("June 6, 2019 12:0:0").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
